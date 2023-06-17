ce7177c704fa5243a639c14d6842e170e62b4476
Tyler Mueller <muelltyl@gmail.com>
2023-06-16 21:56:53 -0500

    converts code to follow new EAV standard

--- mod.ts
+++ mod.ts
@@ -1,10 +1,4 @@
-import { addMsg, Err, isErr, Result } from "./deps.ts";
-import * as Errors from "./errors.ts";
-
-enum NormalizationErrors {
-  InvalidInput = "invalid input",
-  ID0NotSupported = "normalization from ID0",
-}
+import { addMsg, Err, isErr } from "./deps.ts";
 
 export default class SteamIdentifier {
   /** @description The standard Steam id format. STEAM_0:1:XXXXXXXX */
@@ -19,14 +13,29 @@ export default class SteamIdentifier {
    * @implements {normalizeSteamID}
    * @returns A SteamIdentifier object with all the SteamIdentifier values.
    */
+  constructor(normalized: NormalizedSteamIdentifier) {
+    Object.assign(this, normalized);
+  }
   static from(steamid: string) {
     const result = normalizeSteamID(steamid);
     if (isErr(result)) {
       return addMsg(result, "Failed to normalize steamid given: " + steamid);
     }
-    const id = new SteamIdentifier();
-    Object.assign(id, result);
-    return id;
+    return new SteamIdentifier(result);
+  }
+  static fromID64(steamid: string) {
+    const result = normalizeFromID32(steamid);
+    if (isErr(result)) {
+      return addMsg(result, "Failed to normalize steamid given: " + steamid);
+    }
+    return new SteamIdentifier(result);
+  }
+  static fromID32(steamid: string) {
+    const result = normalizeFromID32(steamid);
+    if (isErr(result)) {
+      return addMsg(result, "Failed to normalize steamid given: " + steamid);
+    }
+    return new SteamIdentifier(result);
   }
 }
 
@@ -43,40 +52,36 @@ export type NormalizedSteamIdentifier = {
 
 export function normalizeSteamID(
   steamid: string,
-): Result<
-  NormalizedSteamIdentifier,
-  | Errors.Failure<typeof NormalizationErrors.InvalidInput>
-  | Errors.NotSupported<typeof NormalizationErrors.ID0NotSupported>
-> {
+) {
   const is_id3 = ID3_REGEX.test(steamid);
   const is_numeric = !isNaN(Number(steamid));
   const is_id0 = ID0_REGEX.test(steamid);
   if (is_id0) {
-    return Err("normalization from ID0 is not supported");
+    return Err("ID0 is not supported");
   }
   if (is_id3) {
     return normalizeFromID3(steamid);
   }
   if (is_numeric) {
     if (BigInt(steamid) < MAGIC) {
-      return normalizeFromID32(Number(steamid));
+      return normalizeFromID32(steamid);
     }
     if (BigInt(steamid) > MAGIC) {
-      return normalizeFromID64(BigInt(steamid));
+      return normalizeFromID64(steamid);
     }
   }
-  return Err("Failed because invalid input");
+  return Err("Invalid Input");
 }
 
-export function normalizeFromID32(steamid: number) {
+export function normalizeFromID32(steamid: string) {
   return {
-    id: convertID64toID(BigInt(steamid) + 76561197960265728n),
+    id: convertID64toID(String(BigInt(steamid) + 76561197960265728n)),
     id3: `U:1:${steamid}`,
     id32: String(steamid),
     id64: String(BigInt(steamid) + 76561197960265728n),
   };
 }
-export function normalizeFromID64(steamid: bigint) {
+export function normalizeFromID64(steamid: string) {
   return {
     id: convertID64toID(steamid),
     id3: `U:1:${BigInt(steamid) - 76561197960265728n}`,
@@ -85,10 +90,10 @@ export function normalizeFromID64(steamid: bigint) {
   };
 }
 export function normalizeFromID3(steamid: string) {
-  return normalizeFromID32(Number(steamid.substring(4)));
+  return normalizeFromID32(steamid.substring(4));
 }
-export function convertID64toID(steamid: bigint) {
-  const bin = steamid.toString(2).substring(1, 63).padStart(64, "0");
+export function convertID64toID(steamid: string) {
+  const bin = BigInt(steamid).toString(2).substring(1, 63).padStart(64, "0");
   const x = parseInt(bin.slice(0, 8), 2);
   const y = parseInt(bin.slice(63), 2);
   const z = parseInt(bin.slice(32, 63), 2);
