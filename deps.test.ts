5a82b60fd8b0157f977cb5c983595f2ff5c732f0
Tyler Mueller <muelltyl@gmail.com>
2023-06-16 21:56:22 -0500

    updates EAV to the latest version for tests

--- deps.test.ts
+++ deps.test.ts
@@ -1,3 +1,3 @@
 export * from "./mod.ts";
 export * from "https://deno.land/std@0.192.0/testing/asserts.ts";
-export * from "https://deno.land/x/eav@0.0.1/mod.ts";
+export * from "https://deno.land/x/eav@0.0.2/mod.ts";
