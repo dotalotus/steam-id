7c545a4e08da5a6100d4a096417a1683d8f84521
Tyler Mueller <muelltyl@gmail.com>
2023-06-16 21:55:22 -0500

    removes errors

--- errors.ts
+++
@@ -1,8 +0,0 @@
-import { CustomError } from "./deps.ts";
-
-export type NotSupported<Thing extends string = ""> = CustomError<
-  `${Thing} is not supported`
->;
-export type Failure<Reason extends string = ""> = CustomError<
-  `Failed because ${Reason}`
->;
